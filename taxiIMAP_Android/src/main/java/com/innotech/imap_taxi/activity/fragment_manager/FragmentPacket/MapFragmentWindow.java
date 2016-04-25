c3e80d9654d66ad4e1eb3e57b4bbae350111606f
pphilipp <philippdevel@gmail.com>
2016-04-25 14:12:59 +0300

    Update: MapFragment impl Butter Knife and clean code. Add png for clock.

--- taxiIMAP_Android/src/main/java/com/innotech/imap_taxi/activity/fragment_manager/FragmentPacket/MapFragmentWindow.java
+++ taxiIMAP_Android/src/main/java/com/innotech/imap_taxi/activity/fragment_manager/FragmentPacket/MapFragmentWindow.java
@@ -25,181 +25,95 @@ import com.innotech.imap_taxi.network.packet.Packet;
 import com.innotech.imap_taxi.ping.MyLocationListener;
 import com.innotech.imap_taxi3.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
-
-public class MapFragmentWindow extends FragmentPacket {
-
+public class MapFragmentWindow extends FragmentPacket implements OnClickListener {
+	private static final String LOG_TAG = MapFragmentWindow.class.getSimpleName();
 	private GoogleMap mMap;
 	LatLng myLoc;
 	MarkerOptions myLoMmarker;
-
-	Button btnMe, btnBack;
-    ImageButton zoomIn, zoomOut;
-
+	@Bind(R.id.btn_show_me) Button btnMe;
+    @Bind(R.id.btn_zoom_in) ImageButton zoomIn;
+	@Bind(R.id.btn_zoom_out)ImageButton zoomOut;
 	boolean updateMyLoc;
 	View myView = null;
-
     ArrayList<LatLng> lg;
-
-    float lat, lon;
+    float lat;
+	float lon;
     public static int orderId = -1;
 
 	public MapFragmentWindow() {
 		super(MAP);
 	}
+
 	@Override
 	public void onSaveInstanceState(Bundle outState) {
 		super.onSaveInstanceState(outState);
+		Log.d(LOG_TAG, "onSaveInstanceState()");
 		setUserVisibleHint(true);
 	}
 
     @Override
     public void onViewStateRestored(Bundle savedInstanceState) {
         super.onViewStateRestored(savedInstanceState);
+		Log.d(LOG_TAG, "onViewStateRestored()");
     }
 
     @Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        Log.wtf("MyMap", "onCreateView");
-
-        if (myView!=null)
-		{
+		Log.d(LOG_TAG, "onCreateView()");
+        if (myView != null) {
 			((ViewGroup)myView.getParent()).removeAllViews();
             return myView;
 		}
 
-
-		try{
 			myView  = inflater.inflate(R.layout.map_fragment_new, container, false);
+			ButterKnife.bind(this, myView);
+			btnMe.setOnClickListener(this);
+			zoomIn.setOnClickListener(this);
+			zoomOut.setOnClickListener(this);
 
+			routesAnswerPacketListenerImpl();
 
-            btnMe = (Button) myView.findViewById(R.id.btn_show_me);
-//			btnBack = (Button) view.findViewById(R.id.btn_back);
-            zoomIn = (ImageButton) myView.findViewById(R.id.btn_zoom_in);
-            zoomOut = (ImageButton) myView.findViewById(R.id.btn_zoom_out);
-
-//			btnBack.setOnClickListener(new OnClickListener() {
-//
-//				@Override
-//				public void onClick(View arg0) {
-//					FragmentTransactionManager.getInstance().back();
-//				}
-//			});
-
-			btnMe.setOnClickListener(new OnClickListener() {
-
-				@Override
-				public void onClick(View arg0) {
-					if (mMap != null && MyLocationListener.lat != 0.0 && MyLocationListener.lon != 0.0) {
-						mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(MyLocationListener.lat, MyLocationListener.lon),15), 1500, null);
-                        Marker marker=mMap.addMarker(new MarkerOptions().
-                                position(new LatLng(MyLocationListener.lat,MyLocationListener.lon)).
-                                draggable(false).
-                                title("Вы здесь").
-                                icon(BitmapDescriptorFactory.fromResource(R.drawable.driver_green)));
-                                marker.showInfoWindow();
-                     } else {
-						Toast.makeText(ContextHelper.getInstance().getCurrentContext(), "Идет поиск ...", Toast.LENGTH_SHORT).show();
-					}
-				}
-			});
-
-            zoomIn.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(mMap != null){
-                        mMap.animateCamera(CameraUpdateFactory.zoomIn());
-                    }
-                }
-            });
-            zoomOut.setOnClickListener(new OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if(mMap != null){
-                        mMap.animateCamera(CameraUpdateFactory.zoomOut());
-                    }
-                }
-            });
-
- /*           MapFragment mapFragment = (MapFragment)getFragmentManager().findFragmentById(R.id.map);
-            mapFragment.getMapAsync(this);
-*/
-/*            try {
-                mMap = ((MapFragmentWindow) getFragmentManager()
-                        .findFragmentById(R.id.map)).getMapAsync(this);
-                CameraPosition cameraPosition = new CameraPosition.Builder()
-                        .target(new LatLng(50.4029361, 30.3883177))
-                        .zoom(6)
-                        .build();
-                Log.d("Map", "CameraCreate");
-                CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
-                mMap.animateCamera(cameraUpdate);
-            } catch (Exception e) {
-                Log.e("Map", "Map create error = " + e.getMessage());
-                e.printStackTrace();
-            }
-*/
-/*			if (mMap == null) {
-				mMap = ((SupportMapFragment) getFragmentManager().findFragmentById(
-						R.id.map)).getMap();
-                mMap.setMyLocationEnabled(true);
-/*
-				if (mMap != null) {
-                    LatLng Kyev = new LatLng(50.4029361, 30.3883177);
-                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(Kyev, 10));
-                    mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-                    mMap.setMyLocationEnabled(true);
-
-//					mMap.moveCamera(CameraUpdateFactory.newLatLng(new LatLng(48.6, 32)));
-//					mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(48.6, 32),6), 100, null);
-
-                    mMap.getUiSettings().setZoomControlsEnabled(false);
-
-					myLoMmarker = new MarkerOptions().position(new LatLng(0, 0)).title("Это я");
-					myLoMmarker.visible(true);
-					mMap.addMarker(myLoMmarker);
-				}
-			}*/
-
-			MultiPacketListener.getInstance().addListener(Packet.GET_ROUTES_ANSWER, new OnNetworkPacketListener() {
-				@Override
-				public void onNetworkPacket(Packet packet) {
-					GetRoutesResponse pack = (GetRoutesResponse) packet;
-					Log.e("IMAP", "!!! goted GET_ROUTES_ANSWER " + pack.orderId + " x:" + pack.geoX.size() + " y:" + pack.geoY.size());
-
-					if (pack.geoX.size()>0) {
-
-						lg = new ArrayList<LatLng>();
-						for (int i = 0; i < pack.geoY.size(); i++) {
-							lg.add(new LatLng(pack.geoY.get(i), pack.geoX.get(i)));
-						}
-
-						ContextHelper.getInstance().runOnCurrentUIThread(
-								new Runnable() {
-									public void run() {
-										updateMyLoc = false;
-
-										drawRouteIfExist(lg);
-									}
-								});
-					}
-				}
-			});
-
-		}
-		catch (Exception e){
-            e.printStackTrace();
-            Log.getStackTraceString(e);
-//            Log.e("MApFragment Exception", e.getMessage());
-        }
 		return myView;
-
-
     }
 
-	public void drawRouteIfExist(ArrayList<LatLng> l) {
+	private void routesAnswerPacketListenerImpl() {
+		MultiPacketListener.getInstance().addListener(
+				Packet.GET_ROUTES_ANSWER,
+				new OnNetworkPacketListener() {
+					@Override
+					public void onNetworkPacket(Packet packet) {
+						GetRoutesResponse pack = (GetRoutesResponse) packet;
+						Log.e("IMAP", "!!! goted GET_ROUTES_ANSWER "
+								+ pack.orderId + " x:" + pack.geoX.size()
+								+ " y:" + pack.geoY.size());
 
-		Log.v("ABC", "size - " + l.size() + "first point - " + l.get(0).latitude + " " + l.get(0).longitude);
+						if (pack.geoX.size()>0) {
+							lg = new ArrayList<LatLng>();
+
+							for (int i = 0; i < pack.geoY.size(); i++) {
+								lg.add(new LatLng(pack.geoY.get(i), pack.geoX.get(i)));
+							}
+
+							ContextHelper.getInstance().runOnCurrentUIThread(
+									new Runnable() {
+										public void run() {
+											updateMyLoc = false;
+											drawRouteIfExist(lg);
+										}
+									});
+						}
+					}
+				});
+	}
+
+	public void drawRouteIfExist(ArrayList<LatLng> l) {
+		Log.d(LOG_TAG, "drawRouteIfExist()" + "\n"
+				+ " size - " + l.size() + "\n"
+				+ "first point - " + l.get(0).latitude + "\n"
+				+ " " + l.get(0).longitude);
 
 		mMap.addMarker(new MarkerOptions().position(l.get(0)).title("Начало")
 				.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_ROSE)));
@@ -208,22 +122,19 @@ public class MapFragmentWindow extends FragmentPacket {
 				.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
 
 		mMap.addPolyline(new PolylineOptions().addAll(l).color(Color.BLUE));
-//		mMap.moveCamera(CameraUpdateFactory.newLatLng(l.get(0)));
-		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(l.get(0),16), 2000, null);
+		mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(l.get(0), 16), 2000, null);
 	}
 
 	@Override
 	public void onResume() {
 		super.onResume();
 		Log.d("MyMap", "MapFragmentWindow onResume");
-		Log.d("catchOnResume", "fragId = Map");
-        Log.wtf("Map", "onResume");
 
         try {
             mMap = ((SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map)).getMap();
             CameraPosition cameraPosition = new CameraPosition.Builder()
                     .target(new LatLng(50.4327633,30.597208))
-                    .zoom(8)
+                    .zoom(10)
                     .build();
             Log.d("MyMap", "CameraCreate");
             CameraUpdate cameraUpdate = CameraUpdateFactory.newCameraPosition(cameraPosition);
@@ -234,37 +145,29 @@ public class MapFragmentWindow extends FragmentPacket {
             mMap.addMarker(myLoMmarker);
         }
         catch (Exception e) {
-            Log.e("MyMap", "Map onResume create error = " + e.getMessage());
+            Log.d(LOG_TAG, "Map onResume create error = " + e.getMessage());
             e.printStackTrace();
         }
-
 
         if(orderId != -1){
             Log.wtf("Map", "requestRoute");
             RequestHelper.getRoutes(orderId);
         }
+
 		updateMyLoc = true;
 
 		if (mMap != null && ServerData.getInstance().gpsOrNetProv) {
-
-			//Log.e("btnTest", "btnMap loc upd");
-
 			new Thread(new Runnable() {
-
 				@Override
 				public void run() {
-
 					while (updateMyLoc) {
                         try {
                             Thread.sleep(10000);
                         } catch (InterruptedException e) {
-                            // TODO Auto-generated catch block
                             e.printStackTrace();
                         }
 						if (MyLocationListener.lat != 0.0
 								&& MyLocationListener.lon != 0.0) {
-
-
 							myLoc = new LatLng(
 									MyLocationListener.lat,
 									MyLocationListener.lon);
@@ -280,13 +183,8 @@ public class MapFragmentWindow extends FragmentPacket {
 										}
 									});
 						}
-
-
 					}
-
-
 				}
-
 			}).start();
 		}
 	}
@@ -308,8 +206,37 @@ public class MapFragmentWindow extends FragmentPacket {
     @Override
 	public void onDestroy() {
 		super.onDestroy();
-        Log.wtf("Map", "onDestroy");
+		ButterKnife.unbind(this);
+        Log.d(LOG_TAG, "onDestroy()");
         myView = null;
         mMap = null;
+	}
+
+	@Override
+	public void onClick(View v) {
+		switch (v.getId()) {
+			case R.id.btn_show_me:
+				if (mMap != null && MyLocationListener.lat != 0.0 && MyLocationListener.lon != 0.0) {
+					mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(MyLocationListener.lat,
+							MyLocationListener.lon), 15), 1500, null);
+					Marker marker = mMap.addMarker(new MarkerOptions().
+							position(new LatLng(MyLocationListener.lat, MyLocationListener.lon)).
+							draggable(false).
+							title("Вы здесь").
+							icon(BitmapDescriptorFactory.fromResource(R.drawable.driver_green)));
+					marker.showInfoWindow();
+				} else {
+					Toast.makeText(ContextHelper.getInstance().getCurrentContext(), "Идет поиск ...", Toast.LENGTH_SHORT).show();
+				}break;
+			case R.id.btn_zoom_in :
+				if(mMap != null)
+					mMap.animateCamera(CameraUpdateFactory.zoomIn());
+				break;
+			case R.id.btn_zoom_out :
+				if(mMap != null)
+					mMap.animateCamera(CameraUpdateFactory.zoomOut());
+				break;
+			default: break;
+		}
 	}
 }
